5c0cd085a4ff6a8e40c4cf94a0a512aaf07c1708
Ravi Mamidipalli <ravi.mamidipalli@gmail.com>
2016-07-01 17:57:15 +0530

    Remove comments

--- glance-pub/glance-pub.js
+++ glance-pub/glance-pub.js
@@ -2,15 +2,15 @@ console.log("*** Publish ***");
 
   var pubnub = require("pubnub")({
     ssl           : true,  // <- enable TLS Tunneling over TCP
-    publish_key   : "pub-c-c27df9ed-a723-4fd3-b47d-0fad432bc885",
-    subscribe_key : "sub-c-69bd7168-5383-11e5-8a9f-0619f8945a4f"	
+    publish_key   : "",
+    subscribe_key : ""	
 	
     });
  
 exports.handler = function(event, context) {
     
     console.log("In Handler:"+event.key1);
-    var message = { "msg" : "Hello Vishnu" };
+    var message = { "msg" : "Hello World" };
     pubnub.publish({ 
         channel   : 'my_channel',
         message   : message,
